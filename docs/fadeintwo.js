872f193ebf40ee032b82b95526e9eb5c104d62cd
allison98 <allison98@gmail.com>
2018-02-02 11:39:43 -0800

    working site

--- docs/fadeintwo.js
+++ docs/fadeintwo.js
@@ -3,22 +3,19 @@ $(document).ready(function () {
 
     /* Every time the window is scrolled ... */
     $(window).scroll(function () {
+        $('.hideme3').each(function (i) {
 
-        /* Check the location of each desired element */
-        $('.hideme').each(function (i) {
-
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var top_of_object = $(this).offset().top
             var bottom_of_window = $(window).scrollTop() + $(window).height();
 
             /* If the object is completely visible in the window, fade it it */
-            if (bottom_of_window > bottom_of_object) {
+            if (bottom_of_window < top_of_object) {
 
-                $(this).animate({ 'opacity': '1' }, 1500);
+                $(this).delay(200).animate({ 'opacity': '1' }, 500);
 
             }
 
         });
-
     });
 
 });
