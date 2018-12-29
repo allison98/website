d600bbf486c3a46b66dbd3a6d823dff0e76d570d
allison98 <allison98@gmail.com>
2018-12-29 16:48:44 -0600

    fixes

--- meweb/meweb/Default.aspx
+++
@@ -1,2 +0,0 @@
-﻿<%@ Page Title="Home Page" Language="VB" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="Default.aspx.vb" Inherits="_Default" %>
-
--- meweb/meweb/Default.aspx.vb
+++
@@ -1,8 +0,0 @@
-﻿
-Partial Class _Default
-    Inherits Page
-
-    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
-
-    End Sub
-End Class
